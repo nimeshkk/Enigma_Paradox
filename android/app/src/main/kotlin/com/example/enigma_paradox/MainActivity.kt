136dae4190d5c85b8076a9dbe288cb59ced079b6
nimeshkk <nimeshkliyanage@gmail.com>
2023-08-16 14:24:46 +0530

    update

---
+++ android/app/src/main/kotlin/com/example/enigma_paradox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.enigma_paradox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/signin.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: const Center(
+        child: Text(
+          'Sign In',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
